ef02206745d97aeb82f372159034c3e3be113d82
IT24100527 <it24100527@my.sliit.lk>
2026-09-22 18:40:41 +0530

    fix(ride-management): resolve embedded mongo and mock dependencies for test suite

--- ride-management-service/src/test/java/com/ridelink/ride/controller/RideControllerTest.java
+++ ride-management-service/src/test/java/com/ridelink/ride/controller/RideControllerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.bean.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,6 +33,9 @@ class RideControllerTest {
 
     @MockBean
     private RideService rideService;
+
+    @MockBean
+    private org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory;
 
     private String generateToken(String email, String role, String userId) {
         return io.jsonwebtoken.Jwts.builder()
